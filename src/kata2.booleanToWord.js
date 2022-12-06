d7e030031a63dc8790b8c441280edcb34ae44386
tsv-stacks-linux <tsvirdi@yahoo.co.uk>
2022-12-06 21:25:24 +0000

    completed 2 /7

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,3 +1,11 @@
-const booleanToWord = (boolean) => {};
+const booleanToWord = (boolean) => {
+  if (boolean === true) {
+    return "Yes";
+  }
+  if (boolean === false) {
+    return "No";
+  }
+  return "Not a Boolean";
+};
 
 module.exports = booleanToWord;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -2,4 +2,19 @@ const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
   // how do we create specs again???
+  test("check parameter boolean true returns string Yes", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+  });
+
+  test("check parameter boolean false returns string No", () => {
+    expect(booleanToWord(false)).toBe("No");
+  });
+
+  test("check parameter string true return not a boolean", () => {
+    expect(booleanToWord("true")).toBe("Not a Boolean");
+  });
+
+  test("check parameter string false return not a boolean", () => {
+    expect(booleanToWord("false")).toBe("Not a Boolean");
+  });
 });
